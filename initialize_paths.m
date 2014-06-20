cb72312ef53c629fe21e26ef7de34fdde312d8ba
Liz Kellogg <lizkellogg@gmail.com>
2014-06-20 16:41:22 -0700

    add function to initialize paths

---
+++ initialize_paths.m
@@ -0,0 +1,7 @@
+function[] = initialize_paths(matlab_dir)
+    addpath([matlab_dir 'export_fig'])
+    addpath([matlab_dir 'basic'])
+    addpath([matlab_dir 'EMIODist'])
+    addpath([matlab_dir 'MRCIO'])
+    addpath([matlab_dir 'Solvation_Demo'])
+end
